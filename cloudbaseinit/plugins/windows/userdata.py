2c6a4156160bc3e345298acb8c8bd479bcb77389
Georgy Okrokvertskhov <gokrokvertskhov@mirantis.com>
2013-05-13 15:28:31 -0700

    Add support for MIME encoded multi-part user-data. This is an initial dirty fix to support MIME user data from HEAT. HEAT introduces new part types which are not defined in original cloud-init.

--- cloudbaseinit/plugins/windows/userdata.py
+++ cloudbaseinit/plugins/windows/userdata.py
@@ -17,21 +17,71 @@
 import re
 import tempfile
 import uuid
+import email
+import tempfile
+import os
+import errno
 
+from cloudbaseinit.openstack.common import cfg
 from cloudbaseinit.openstack.common import log as logging
 from cloudbaseinit.osutils.factory import *
 from cloudbaseinit.plugins.base import *
+from cloudbaseinit.plugins.windows.userdata_plugins import PluginSet
 
 LOG = logging.getLogger(__name__)
 
+opts = [
+    cfg.StrOpt('user_data_folder', default='cloud-data',
+        help='Specifies a folder to store multipart data files.'),
+    ]
+
+CONF = cfg.CONF
+CONF.register_opts(opts)
 
 class UserDataPlugin():
+    def __init__(self, cfg=CONF):
+        self.cfg = cfg
+        self.msg = None
+        self.plugin_set = PluginSet()
+        self.plugin_set.reload()
+        return
+
     def execute(self, service):
         user_data = service.get_user_data('openstack')
         if not user_data:
             return False
 
         LOG.debug('User data content:\n%s' % user_data)
+
+        if user_data.startswith('Content-Type: multipart'):
+            for part in self.parse_MIME(user_data):
+                self.process_part(part)
+        else:
+            self.handle(user_data)
+        return
+
+    def process_part(self, part):
+        if part.get_filename() == 'cfn-userdata':
+            self.handle(part.get_payload())
+        return
+
+    def parse_MIME(self, user_data):
+        folder = self.cfg.user_data_folder
+        self.create_folder(folder)
+
+        self.msg = email.message_from_string(user_data)
+        return self.msg.walk()
+
+
+    def create_folder(self, folder):
+        try:
+            os.mkdir(folder)
+        except os.OSError, e:
+            if e.errno != errno.EEXIST:
+                raise e
+        return
+
+    def handle(self, user_data):
 
         osutils = OSUtilsFactory().get_os_utils()
 
